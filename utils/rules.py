f1fc5a7effbdb37f41991179986d52501e99269a
Eric <88685021+pandabear189@users.noreply.github.com>
2021-09-27 21:47:47 -0500

    Create rules.py

---
+++ utils/rules.py
@@ -0,0 +1,8 @@
+RULES = [
+    "⌬ Respect **ALL** individuals in this server",
+    "⌬ No profanity or inappropriate language, content, or links.",
+    "⌬ Do not spam or flood the chat with an excessive amount of repetitive messages",
+    "⌬ Do not self-promote",
+    "⌬ Do not harass other members",
+    "⌬ Use good judgment when deciding what content to leave in and take out. As a general rule of thumb: **When in doubt, leave it out.**",
+]
