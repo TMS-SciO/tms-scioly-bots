8985dea2c93753e3aa3bacab8db8a6e723059953
Eric <88685021+pandabear189@users.noreply.github.com>
2021-09-27 20:23:20 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,56 @@
+# ROLES
+ROLE_SELFMUTE = "Self-Muted"
+ROLE_MUTED = "Muted"
+ROLE_MR = "Member"
+ROLE_TRIAL = "Trial [SL]"
+ROLE_VIP = "VIP"
+ROLE_SERVERLEADER = "Server Leader"
+ROLE_COACH = "Coach"
+
+# ROLES FOR VIEWS
+ROLE_AP = "Anatomy and Physiology"
+ROLE_BPL = "Bio Process Lab"
+ROLE_DD = "Disease Detectives"
+ROLE_GG = "Green Generation"
+ROLE_O = "Ornithology"
+ROLE_DP = "Dynamic Planet"
+ROLE_M = "Meteorology"
+ROLE_RS = "Road Scholar"
+ROLE_RM = "Rocks and Minerals"
+ROLE_SS = "Solar System"
+ROLE_CTW = "Crave the Wave"
+ROLE_SOM = "Sounds of Music"
+ROLE_STC = "Storm the Castle"
+ROLE_FS = "Food Science"
+ROLE_CB = "Crime Busters"
+ROLE_B = "Bridges"
+ROLE_EWS = "Electric Wright Stuff"
+ROLE_MP = "Mission Possible"
+ROLE_MV = "Mousetrap Vehicle"
+ROLE_C = "Codebusters"
+ROLE_ED = "EXP Design"
+ROLE_PPP = "Ping Pong Parachute"
+ROLE_WIDI = "WIDI"
+ROLE_HE = "He/Him"
+ROLE_SHE = "She/Her"
+ROLE_THEY = "They/Them"
+ROLE_ASK = "Ask"
+ROLE_AE = "All Events"
+
+
+# CHANNELS
+CHANNEL_DEV = "tms-bot-dev"
+CHANNEL_ROLES = "roles"
+CHANNEL_BOTSPAM = "bot-spam"
+CHANNEL_RULES = "rules"
+CHANNEL_REPORTS = "bot-report-channel"
+CHANNEL_LEAVE = "member-leave"
+CHANNEL_DELETEDM = "moderation-logging"
+CHANNEL_EDITEDM = "moderation-logging"
+CHANNEL_DMLOG = "bot-dm-log"
+WELCOME_CHANNEL = "general"
+CHANNEL_UNSELFMUTE = "un-self-mute"
+
+# OTHER
+SERVER_ID = 816806329925894217
+TMS_BOT_IDS = [865671215179366410, 870741665294467082]
