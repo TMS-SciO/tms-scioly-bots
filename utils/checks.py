02126c8a15852358ba9a53682c36b10e8333f30c
Eric <88685021+pandabear189@users.noreply.github.com>
2021-09-28 21:09:13 -0500

    Update checks.py

--- utils/checks.py
+++ utils/checks.py
@@ -1,18 +1,23 @@
 import discord
-from variables import ROLE_COACH, ROLE_SERVERLEADER
+from variables import *
 import asyncio
 import datetime
-
+from discord.ext import commands
 
 STOPNUKE = datetime.datetime.utcnow()
 
 
-async def is_staff(bot, ctx):
-    """Checks to see if the user is a staff member."""
-    member = ctx.message.author
-    staffRole = discord.utils.get(member.guild.roles, name=ROLE_SERVERLEADER)
-    coachRole = discord.utils.get(member.guild.roles, name=ROLE_COACH)
-    return staffRole in member.roles or coachRole in member.roles
+def is_staff():
+    """Checks to see if the author of ctx message is a staff member."""
+    def predicate(ctx):
+        guild = ctx.bot.get_guild(SERVER_ID)
+        member = guild.get_member(ctx.message.author.id)
+        staffRole = discord.utils.get(guild.roles, name=ROLE_SERVERLEADER)
+        coachRole = discord.utils.get(guild.roles, name=ROLE_COACH)
+        if any(r in [staffRole, coachRole] for r in member.roles):
+            return True
+        raise discord.ext.commands.MissingAnyRole([staffRole, coachRole])
+    return commands.check(predicate)
 
 
 async def _nuke_countdown(ctx, count=-1):
@@ -29,6 +34,16 @@ async def _nuke_countdown(ctx, count=-1):
         await asyncio.sleep(1)
         if STOPNUKE > datetime.datetime.utcnow():
             return await ctx.send("A COMMANDER HAS PAUSED ALL NUKES FOR 20 SECONDS. NUKE CANCELLED.")
+
+
+
+
+# async def is_staff(bot):
+#     """Checks to see if the user is a staff member."""
+#     member = ctx.message.author
+#     staffRole = discord.utils.get(member.guild.roles, name=ROLE_SERVERLEADER)
+#     coachRole = discord.utils.get(member.guild.roles, name=ROLE_COACH)
+#     return staffRole in member.roles or coachRole in member.roles
 
 
 # def not_blacklisted_channel(blacklist):
