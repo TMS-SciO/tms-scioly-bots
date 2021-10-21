65314b9c301268844abd3c2999e16a91d4aff617
Eric <88685021+pandabear189@users.noreply.github.com>
2021-10-21 13:36:32 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -85,7 +85,7 @@ class General(commands.Cog):
         return Counter(m.author.display_name for m in deleted)
 
     @commands.command()
-    async def source(self, ctx, command=None):
+    async def source(self, ctx, *, command: str = None):
         """Displays my full source code or for a specific command.
         To display the source code of a subcommand you can separate it by
         periods, e.g. tag.create for the create subcommand of the tag command
@@ -94,27 +94,34 @@ class General(commands.Cog):
         source_url = 'https://github.com/pandabear189/tms-scioly-bots'
         branch = 'main'
         if command is None:
-            await ctx.send(source_url)
+            return await ctx.send(source_url)
 
+        if command == 'help':
+            src = type(self.bot.help_command)
+            module = src.__module__
+            filename = inspect.getsourcefile(src)
         else:
             obj = self.bot.get_command(command.replace('.', ' '))
             if obj is None:
                 return await ctx.send('Could not find command.')
 
+            # since we found the command we're looking for, presumably anyway, let's
+            # try to access the code itself
             src = obj.callback.__code__
             module = obj.callback.__module__
             filename = src.co_filename
 
-            lines, firstlineno = inspect.getsourcelines(src)
-            if not module.startswith('discord'):
-                location = os.path.relpath(filename).replace('\\', '/')
-            else:
-                location = module.replace('.', '/') + '.py'
-                source_url = 'https://github.com/Rapptz/discord.py'
-                branch = 'master'
+        lines, firstlineno = inspect.getsourcelines(src)
+        if not module.startswith('discord'):
+            # not a built-in command
+            location = os.path.relpath(filename).replace('\\', '/')
+        else:
+            location = module.replace('.', '/') + '.py'
+            source_url = 'https://github.com/Rapptz/discord.py'
+            branch = 'master'
 
-            final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
-            await ctx.send(final_url)
+        final_url = f'<{source_url}/blob/{branch}/{location}#L{firstlineno}-L{firstlineno + len(lines) - 1}>'
+        await ctx.send(final_url)
 
     @commands.command()
     async def rule(self, ctx, number:Literal["1", "2", "3", "4", "5", "6"] = commands.Option(description="Which rule to display")):
@@ -611,7 +618,6 @@ class General(commands.Cog):
         await ctx.send(embed=e)
 
     # @commands.command()
-    # TODO FIX THIS
     # async def selfmute(self, ctx, time):
     #     """
     #     Self mutes the user that invokes the command.
