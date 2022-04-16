3e9d9edd4820fb521f23cfc17787246a05da244c
Eric <88685021+pandabear189@users.noreply.github.com>
2022-04-16 11:49:54 -0500

    Update listeners.py

--- cogs/listeners.py
+++ cogs/listeners.py
@@ -1,11 +1,23 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import discord
 from discord.ext import commands
 
-from utils.variables import *
+from utils import (
+    SERVER_ID,
+    Role,
+    Channel,
+    TMS_BOT_IDS
+)
+
+if TYPE_CHECKING:
+    from bot import TMS
 
 
 class Listeners(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: TMS):
         self.bot = bot
 
     @commands.Cog.listener()
@@ -35,8 +47,8 @@ class Listeners(commands.Cog):
             reporter_cog: commands.Cog = self.bot.get_cog('Reporter')
             await reporter_cog.create_inappropriate_username_report(member, member.name)
 
-        role: discord.Role = discord.utils.get(member.guild.roles, name=ROLE_MR)
-        join_channel: discord.TextChannel = discord.utils.get(member.guild.text_channels, id=WELCOME_CHANNEL)
+        role: discord.Role = member.guild.get_role(Role.MEMBER)
+        join_channel: discord.TextChannel = self.bot.get_channel(Channel.WELCOME)
         await member.add_roles(role)
         embed = discord.Embed(
             title="Welcome!",
@@ -52,275 +64,128 @@ class Listeners(commands.Cog):
         )
 
     @commands.Cog.listener()
-    async def on_raw_message_edit(self, payload):
-        await self.log_edit_message_payload(payload)
+    async def on_message_edit(self, before, after):
+        if after.author.id in TMS_BOT_IDS:
+            return
+        if not after.guild.id == SERVER_ID:
+            return
+        if after.channel.type != discord.ChannelType.private and after.channel.id in (
+                Channel.EDITEDM,
+                Channel.DELETEDM,
+                Channel.DELETEDM
+        ):
+            return
+        censor_cog = self.bot.get_cog("Censor")
+        censor_found = censor_cog.censor_needed(after.content)
+        if censor_found:
+            await after.delete()
+        message_embed = discord.Embed(
+            title="\U0000270f Edited Message",
+            color=discord.Color.blurple()
+        )
+        message_embed.add_field(
+            name="Author", value=after.author.mention, inline=True
+        )
+        message_embed.add_field(
+            name="Message ID", value=after.id, inline=True
+        )
+        message_embed.add_field(
+            name="Message Link", value=f"[Here!]({after.jump_url})", inline=False
+        )
+        message_embed.add_field(
+            name="Before", value=before.content[:1024] if len(before.content) > 0 else "None", inline=True
+        )
+        message_embed.add_field(
+            name="After", value=after.content[:1024] if len(after.content) > 0 else "None", inline=True
+        )
+        message_embed.add_field(
+            name="Attachments",
+            value=" | ".join(
+                [f"**{a.filename}**: [Link]({a.url})" for a in after.attachments]) if len(
+                after.attachments) > 0 else "None", inline=False
+        )
+        message_embed.add_field(
+            name="Embeds",
+            value="\n".join([str(e.to_dict()) for e in after.embeds]) if len(
+                after.embeds) > 0 else "None", inline=False
+        )
+        message_embed.add_field(
+            name="Created", value=discord.utils.format_dt(before.created_at, 'R'), inline=True
+        )
+        message_embed.add_field(
+            name="Edited", value=discord.utils.format_dt(after.created_at, 'R'), inline=True
+        )
+        moderation_log = self.bot.get_channel(Channel.DELETEDM)
+        await moderation_log.send(embed=message_embed)
+
+    @staticmethod
+    async def send_to_dm_log(bot: TMS, message: discord.Message):
+        dm_channel = bot.get_channel(Channel.DELETEDM)
+
+        if message.author.id == bot.user.id:
+            title = ":speech_balloon: Outgoing Direct Message"
+            color = discord.Color.fuchsia()
+        else:
+            title = ":speech_balloon: Incoming Direct Message to TMS-Bot"
+            color = discord.Color.brand_green()
+
+        # Create an embed containing the direct message info and send it to the log channel
+        message_embed = discord.Embed(
+            title=title,
+            description=message.content if len(message.content) > 0 else "This message contained no content.",
+            color=color
+        )
+        message_embed.add_field(name="Author", value=message.author.mention, inline=True)
+        message_embed.add_field(name="Message ID", value=message.id, inline=True)
+        message_embed.add_field(name="Sent", value=discord.utils.format_dt(message.created_at, 'R'), inline=True)
+        message_embed.add_field(name="Attachments", value=" | ".join(
+            [f"**{a.filename}**: [Link]({a.url})" for a in message.attachments]) if len(
+            message.attachments) > 0 else "None", inline=True)
+        await dm_channel.send(embed=message_embed)
 
     @commands.Cog.listener()
-    async def on_raw_message_delete(self, payload):
-        return await self.log_delete_message_payload(payload)
-
-    async def log_edit_message_payload(self, payload):
-        """
-        Logs a payload for the 'Edit Message' event.
-        """
-        # Get the required resources for logging
-        channel = self.bot.get_channel(payload.channel_id)
-        guild = self.bot.get_guild(SERVER_ID) if channel.type == discord.ChannelType.private else channel.guild
-        edited_channel = discord.utils.get(guild.text_channels, id=CHANNEL_DELETEDM)
-
-        # Ignore payloads for events in logging channels (which would cause recursion)
-        if channel.type != discord.ChannelType.private and channel.id in [
-            CHANNEL_EDITEDM,
-            CHANNEL_DELETEDM,
-            CHANNEL_DMLOG
-        ]:
+    async def on_message_delete(self, message: discord.Message):
+        if message.author.id in TMS_BOT_IDS:
             return
-
-        # Attempt to log from the cached message if found, else just report on what is available
-        try:
-            message = payload.cached_message
-            if (discord.utils.utcnow() - message.created_at).total_seconds() < 2:
-                # No need to log edit event for a message that was just created
-                return
-
-            if message.author.id == self.bot.user.id:
-                return
-
-            message_now = await channel.fetch_message(message.id)
-            channel_name = f"{message.author.mention}'s DM" if channel.type == discord.ChannelType.private else message.channel.mention
-
-            embed = discord.Embed(
-                title=":pencil: Edited Message",
-                color=discord.Color.blurple()
-            )
-            fields = [
-                {
-                    "name": "Author",
-                    "value": message.author,
-                    "inline": "True"
-                },
-                {
-                    "name": "Channel",
-                    "value": channel_name,
-                    "inline": "True"
-                },
-                {
-                    "name": "Message ID",
-                    "value": f"{payload.message_id} ([jump!]({message_now.jump_url}))",
-                    "inline": "True"
-                },
-                {
-                    "name": "Created At",
-                    "value": discord.utils.format_dt(message.created_at, 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "Edited At",
-                    "value": discord.utils.format_dt(message_now.edited_at, 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "Attachments",
-                    "value": " | ".join([f"**{a.filename}**: [Link]({a.url})" for a in message.attachments]) if len(
-                        message.attachments) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Past Content",
-                    "value": message.content[:1024] if len(message.content) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "New Content",
-                    "value": message_now.content[:1024] if len(message_now.content) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Embed",
-                    "value": "\n".join([str(e.to_dict()) for e in message.embeds]) if len(
-                        message.embeds) > 0 else "None",
-                    "inline": "False"
-                }
-            ]
-            for field in fields:
-                embed.add_field(
-                    name=field['name'],
-                    value=field['value'],
-                    inline=field['inline']
-                )
-
-            await edited_channel.send(embed=embed)
-
-        except Exception:  # No cached message is available
-            message_now = await channel.fetch_message(payload.message_id)
-            embed = discord.Embed(
-                title=":pencil: Edited Message",
-                color=discord.Color.blurple()
-            )
-
-            fields = [
-                {
-                    "name": "Channel",
-                    "value": self.bot.get_channel(payload.channel_id).mention,
-                    "inline": "True"
-                },
-                {
-                    "name": "Message ID",
-                    "value": f"{payload.message_id} ([jump!]({message_now.jump_url}))",
-                    "inline": "True"
-                },
-                {
-                    "name": "Author",
-                    "value": message_now.author,
-                    "inline": "True"
-                },
-                {
-                    "name": "Created At",
-                    "value": discord.utils.format_dt(message_now.created_at, 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "Edited At",
-                    "value": discord.utils.format_dt(message_now.edited_at, 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "New Content",
-                    "value": message_now.content[:1024] if len(message_now.content) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Current Attachments",
-                    "value": " | ".join([f"**{a.filename}**: [Link]({a.url})" for a in message_now.attachments]) if len(
-                        message_now.attachments) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Current Embed",
-                    "value": "\n".join([str(e.to_dict()) for e in message_now.embeds])[:1024] if len(
-                        message_now.embeds) > 0 else "None",
-                    "inline": "False"
-                }
-            ]
-            for field in fields:
-                embed.add_field(
-                    name=field['name'],
-                    value=field['value'],
-                    inline=field['inline']
-                )
-
-            await edited_channel.send(embed=embed)
-
-    async def log_delete_message_payload(self, payload):
-        """
-        Logs a message payload that came from a 'Delete Message' payload.
-        """
-        # Get the required resources
-        channel = self.bot.get_channel(payload.channel_id)
-        guild = self.bot.get_guild(SERVER_ID) if channel.type == discord.ChannelType.private else channel.guild
-        deleted_channel = discord.utils.get(guild.text_channels, id=CHANNEL_DELETEDM)
-
-        if channel.type != discord.ChannelType.private and channel.id in [CHANNEL_DELETEDM,
-                                                                          CHANNEL_REPORTS,
-                                                                          CHANNEL_CLOSED_REPORTS,
-                                                                          CHANNEL_DMLOG,
-                                                                          CHANNEL_EDITEDM]:
+        if not message.guild.id == SERVER_ID:
             return
-
-        try:
-            message = payload.cached_message
-            if message.author == discord.User.bot:
-                return
-            if message.author == self.bot:
-                return
-            channel_name = (f"{message.author.mention}'s DM"
-                            if channel.type == discord.ChannelType.private
-                            else message.channel.mention)
-            embed = discord.Embed(
-                title=":fire: Deleted Message",
-                color=discord.Color.brand_red()
-            )
-            fields = [
-                {
-                    "name": "Author",
-                    "value": message.author,
-                    "inline": "True"
-                },
-                {
-                    "name": "Channel",
-                    "value": channel_name,
-                    "inline": "True"
-                },
-                {
-                    "name": "Message ID",
-                    "value": payload.message_id,
-                    "inline": "True"
-                },
-                {
-                    "name": "Created At",
-                    "value": discord.utils.format_dt(message.created_at, 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "Deleted At",
-                    "value": discord.utils.format_dt(discord.utils.utcnow(), 'R'),
-                    "inline": "True"
-                },
-                {
-                    "name": "Attachments",
-                    "value": " | ".join([f"**{a.filename}**: [Link]({a.url})" for a in message.attachments]) if len(
-                        message.attachments) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Content",
-                    "value": str(message.content)[:1024] if len(message.content) > 0 else "None",
-                    "inline": "False"
-                },
-                {
-                    "name": "Embed",
-                    "value": "\n".join([str(e.to_dict()) for e in message.embeds])[:1024] if len(
-                        message.embeds) > 0 else "None",
-                    "inline": "False"
-                }
-            ]
-            for field in fields:
-                embed.add_field(
-                    name=field['name'],
-                    value=field['value'],
-                    inline=field['inline']
-                )
-
-            await deleted_channel.send(embed=embed)
-
-        except Exception as _:
-
-            embed = discord.Embed(
-                title=":fire: Deleted Message",
-                description="Because this message was not cached, I was unable to retrieve its content before it was deleted.",
-                color=discord.Color.brand_red()
-            )
-            fields = [
-                {
-                    "name": "Channel",
-                    "value": self.bot.get_channel(payload.channel_id).mention,
-                    "inline": "True"
-                },
-                {
-                    "name": "Message ID",
-                    "value": payload.message_id,
-                    "inline": "True"
-                }
-            ]
-            for field in fields:
-                embed.add_field(
-                    name=field['name'],
-                    value=field['value'],
-                    inline=field['inline']
-                )
-
-            await deleted_channel.send(embed=embed)
+        if message.channel.type != discord.ChannelType.private and message.channel.id in (
+                Channel.EDITEDM,
+                Channel.DELETEDM,
+                Channel.DELETEDM
+        ):
+            return
+        message_embed = discord.Embed(
+            title="\U0001f525 Deleted Message",
+            color=discord.Color.brand_red()
+        )
+        message_embed.add_field(
+            name="Author", value=message.author.mention, inline=True
+        )
+        message_embed.add_field(
+            name="Message ID", value=message.id, inline=True
+        )
+        message_embed.add_field(
+            name="Content", value=str(message.content)[:1024] if len(message.content) > 0 else "None", inline=False
+        )
+        message_embed.add_field(
+            name="Attachments", value=" | ".join(
+                [f"**{a.filename}**: [Link]({a.url})" for a in message.attachments]) if message.attachments else "None",
+            inline=False
+        )
+        message_embed.add_field(
+            name="Created", value=discord.utils.format_dt(message.created_at, 'R'), inline=False
+        )
+        message_embed.add_field(
+            name="Deleted", value=discord.utils.format_dt(discord.utils.utcnow(), 'R'), inline=False
+        )
+        message_embed.add_field(
+            name="Embed", value="\n".join([str(e.to_dict()) for e in message.embeds])[:1024] if len(
+                message.embeds) > 0 else "None", inline=False
+        )
+        moderation_log = self.bot.get_channel(Channel.EDITEDM)
+        await moderation_log.send(embed=message_embed)
 
 
-def setup(bot):
-    bot.add_cog(Listeners(bot))
+async def setup(bot: TMS):
+    await bot.add_cog(Listeners(bot))
